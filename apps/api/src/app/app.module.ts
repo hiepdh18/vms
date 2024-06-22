c6932d6f15d5d839ed7776a10713c98480b91b5a
Do Hoang Hiep <do.hoang.hiep@mqsolutions.com.vn>
2024-06-22 19:34:20 +0700

    change import path ws ptz

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
 import { OnvifModule } from '@vms/onvif';
-import { PtzModule } from '@vms/ws-ptz';
+import { PtzModule } from '@vms/api/ws/ptz';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HttpExceptionFilter } from './exception.filter';
